41e403d39f0eb8e6c16d39fd9aaf50a5e4a27a40
Eric Prud'hommeaux <eric@w3.org>
2016-09-11 16:18:08 -0400

    ~ updated tests

--- test/test.js
+++ test/test.js
@@ -10,11 +10,14 @@ describe ("", function () {
 
   [["ShExJ.jsg", "ShExJ_all.json", true],
    ["ShExJ.jsg", "empty.json", true],
-   ["ShExJ.jsg", "bad-noType.json", undefined],
+   ["ShExJ.jsg", "bad-noType.json", "type"],
    ["ShExJ.jsg", "bad-wrongType.json", false],
-   ["ShExJ.jsg", "bad-unknowProperty.json", false]].forEach(function (t) {
+   ["ShExJ.jsg", "bad-unknowProperty.json", "unknownProperty"]].forEach(function (t) {
      var schema = Path.join(Testdir, t[0]);
      var data = Path.join(Testdir, t[1]);
+     var boolResult = t[2] === true ? "pass" : "fail";
+     var errMatch = t[2] !== true && t[2] !== false ? t[2] : "";
+     var result = `${boolResult}` + (errMatch.length ? ` with an error on "${errMatch}"` : "");
      var p, s, errors;
      it(`JSG should correctly parse schema '${schema}'.` , function () {
        p = Parser.parse(FS.readFileSync(schema, "utf8"));
@@ -24,14 +27,19 @@ describe ("", function () {
        s = Schema(p);
      });
      // console.log(s.htmlSerializer().serialize());
-     it(`Validating '${data}' against '${schema}' should ${(t[2] ? "pass" : "fail")}.` ,
+     it(`Validating '${data}' against '${schema}' should ${result}.` ,
         function () {
           try {
 	    errors = s.validator().validate(JSON.parse(FS.readFileSync(data, "utf8")));
-	    // console.log("errors:", errors);
-	    expect(errors.length === 0).to.equal(t[2]);
           } catch (e) {
-	    expect(errors).to.equal(undefined);
+	    expect(e).to.equal("");
+            errors = null;
+          }
+	  // console.log("errors:", errors);
+          if (errors !== null) {
+	    expect(errors.length === 0).to.equal(boolResult === "pass");
+            if (boolResult === "fail" && errMatch.length !== 0 && errors.length !== 0)
+              expect(errors[0].attribute).to.equal(errMatch);
           }
         });
    });
