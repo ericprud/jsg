9e4f6339e96049f1f94984315d06e0cfc9829c2a
Eric Prud'hommeaux <eric@w3.org>
2016-09-11 08:33:32 -0400

    ~ update tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 var FS = require("fs");
+var Path = require("path");
 var expect = require("chai").expect;
 var Parser = require("../lib/jsg.js");
 var Schema = require("../lib/json-grammar.js");
+
+var Testdir = Path.relative("", __dirname);
 
 describe ("A grammar", function () {
 
@@ -10,18 +13,20 @@ describe ("A grammar", function () {
    ["ShExJ.jsg", "bad-noType.json", undefined],
    ["ShExJ.jsg", "bad-wrongType.json", false],
    ["ShExJ.jsg", "bad-unknowProperty.json", false]].forEach(function (t) {
+     var schema = Path.join(Testdir, t[0]);
+     var data = Path.join(Testdir, t[1]);
      var p, s, errors;
-     it("should correctly parse schema '" + t[0] + "'." , function () {
-       p = Parser.parse(FS.readFileSync(t[0], "utf8"));
+     it("should correctly parse schema '" + schema + "'." , function () {
+       p = Parser.parse(FS.readFileSync(schema, "utf8"));
      });
      // console.log(JSON.stringify(p, null, 2));
-     it("should the schema '" + t[0] + "'." , function () {
+     it("should the schema '" + schema + "'." , function () {
        s = Schema(p);
      });
      // console.log(s.htmlSerializer().serialize());
-     it("should validate '" + t[1] + "' against '" + t[0] + "' and " + (t[2] ? "pass" : "fail") + "." , function () {
+     it("should validate '" + data + "' against '" + schema + "' and " + (t[2] ? "pass" : "fail") + "." , function () {
        try {
-	 errors = s.validator().validate(JSON.parse(FS.readFileSync(t[1], "utf8")));
+	 errors = s.validator().validate(JSON.parse(FS.readFileSync(data, "utf8")));
 	 // console.log("errors:", errors);
 	 expect(errors.length === 0).to.equal(t[2]);
        } catch (e) {
