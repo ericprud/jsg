a1186858ee019619c6ccae352a28d9ebd7cbde2e
Eric Prud'hommeaux <eric@w3.org>
2016-10-06 12:22:36 -0400

    ~ reordered to put LSE-breaking stuff at bottom

--- doc/jsg-simple.js
+++ doc/jsg-simple.js
@@ -1,24 +1,242 @@
-function resize () {
-  var width = $(window).width();
-  var size = parseFloat($("#schema").css("font-size"));
-  var cols = width/size/2;
-  var stuff = `${$("#schema").attr("cols")} -> ${cols} width:${width} font-size:${size}`;
-  $("#schema, #data").attr("cols", cols).parent().find("p").text(stuff);
-//  alert(`${$("#schema").attr("cols")} ${width}, ${size}, ${width / size}`);
+// jsg-simple - Simple JSG invoker for HTML.
+// Copyright 2016 Eric Prud'hommeux
+// Release under MIT License.
+
+function load (selector, obj, func) {
+  $(selector).empty();
+  Object.keys(obj).forEach(k => {
+    var li = $('<li><a href="#">' + k + '</li>');
+    li.on("click", () => {
+      func(k, obj[k], li);
+    });
+    $(selector).append(li);
+  });
 }
 
-var addrSchema = `address { street:NAME no:NUM? }
+function clear () {
+  $("#schema textarea").val("");
+  $("#schema .status").text(" ");
+
+  $("#data textarea").val("");
+  $("#data .status").text(" ");
+  $("#data .passes p:first").text("");
+  $("#data .fails p:first").text("");
+  $("#data .passes ul, #data .fails ul").empty();
+
+  $("#results").text("");
+}
+
+function pickSchema (name, schemaTest, elt) {
+  $("#schema textarea").val(schemaTest.schema);
+  $("#schema .status").text(name);
+
+  $("#data textarea").val("");
+  $("#data .status").text(" ");
+  $("#data .passes p:first").text("Passing:");
+  load("#data .passes ul", schemaTest.passes, pickData);
+  $("#data .fails p:first").text("Failing:");
+  load("#data .fails ul", schemaTest.fails, pickData);
+
+  $("#results").text("");
+}
+
+function pickData (name, dataTest, elt) {
+  $("#data textarea").val(JSON.stringify(dataTest, null, "  "));
+  $("#data .status").text(name);
+  validate();
+}
+
+function validate () {
+  var schemaText = $("#schema textarea").val();
+  var parsed = parser.parse(schemaText);
+  var schema = Schema(parsed);
+  var data = JSON.parse($("#data textarea").val());
+  var errors = schema.validator().validate(data);
+  $("#results").text(errors.join("\n"));
+}
+
+$("#data .passes ul, #data .fails ul").empty();
+$("#validate").on("click", validate);
+$("#clear").on("click", clear);
+// prepareDemos() is invoked after these variables are assigned:
+var addrSchema, perAddrSchema, shexjSchema;
+
+function prepareDemos () {
+  var demos = {
+    "address": {
+      schema: addrSchema,
+      passes: {
+        "complete": { "street":"Elm", "no":"123b" },
+        "just name": { "street":"Elm" },
+      },
+      fails: {
+        "missing street": { "no": "123" },
+        "extra property": { "street":"Elm", "no":"123b", "x": "y" },
+        "misspelled property": { "street999":"Elm", "no":"123b" }
+      }
+    },
+    "person with address": {
+      schema: perAddrSchema,
+      passes: {
+        "no addrs": { "name": "Bob" },
+        "empty addresses": { "name": "Bob", "addrs": [] },
+        "1 address": { "name": "Bob", "addrs": [
+          { "street":"Elm", "no":"123b" }
+        ] },
+        "2 addresses": { "name": "Bob", "addrs": [
+          { "street":"Elm", "no":"123b" },
+          { "street":"Forest", "no":"18" }
+        ] },
+      },
+      fails: {
+        "missing name": { "addrs": [
+          { "street":"Elm", "no":"123b" }
+        ] },
+        "bad address": { "name": "Bob", "addrs": [
+          { "street":"Elm", "no":"123b", "x": "y" }
+        ] },
+        "bad last addresses": { "name": "Bob", "addrs": [
+          { "street":"Elm", "no":"123b" },
+          { "no":"18" }
+        ] },
+      }
+    },
+    "ShExJ": {
+      schema: shexjSchema,
+      passes: {
+        "1card25":{
+          "type": "Schema",
+          "shapes":{
+            "http://a.example/S1": {
+              "type": "Shape",
+              "expression": {
+                "type": "TripleConstraint",
+                "predicate": "http://a.example/p1",
+                "min": 2, "max": 5
+              }
+            }
+          }
+        },
+        "1bnodeRefORRefMinlength": {
+          "type": "Schema",
+          "shapes": {
+            "http://a.example/S1": {
+              "type": "Shape",
+              "expression": {
+                "type": "TripleConstraint",
+                "predicate": "http://a.example/p1",
+                "valueExpr": {
+                  "type": "ShapeOr",
+                  "shapeExprs": [
+                    {
+                      "type": "ShapeAnd",
+                      "shapeExprs": [
+                        {
+                          "type": "NodeConstraint",
+                          "nodeKind": "bnode"
+                        },
+                        {
+                          "type": "ShapeRef",
+                          "reference": "http://a.example/S1"
+                        }
+                      ]
+                    },
+                    {
+                      "type": "ShapeAnd",
+                      "shapeExprs": [
+                        {
+                          "type": "NodeConstraint",
+                          "minlength": 5
+                        },
+                        {
+                          "type": "ShapeRef",
+                          "reference": "http://a.example/S1"
+                        }
+                      ]
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        }
+      },
+      fails: {
+        "misplaced attr":{
+          "type": "Schema",
+          "shapes":{
+            "http://a.example/S1": {
+              "type": "Shape", "max": 5,
+              "expression": {
+                "type": "TripleConstraint",
+                "predicate": "http://a.example/p1",
+                "min": 2
+              }
+            }
+          }
+        },
+        "wrong type": {
+          "type": "Schema",
+          "shapes": {
+            "http://a.example/S1": {
+              "type": "Shape",
+              "expression": {
+                "type": "TripleConstraint",
+                "predicate": "http://a.example/p1",
+                "valueExpr": {
+                  "type": "ShapeOr",
+                  "shapeExprs": [
+                    {
+                      "type": "ShapeAnd",
+                      "shapeExprs": [
+                        {
+                          "type": "NodeConstraint",
+                          "nodeKind": "bnode"
+                        },
+                        {
+                          "type": "ShapeRef",
+                          "reference": "http://a.example/S1"
+                        }
+                      ]
+                    },
+                    {
+                      "type": "Inclusion",
+                      "shapeExprs": [
+                        {
+                          "type": "NodeConstraint",
+                          "minlength": 5
+                        },
+                        {
+                          "type": "ShapeRef",
+                          "reference": "http://a.example/S1"
+                        }
+                      ]
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+  load("#schema .examples ul", demos, pickSchema);
+}
+
+// Large constants with demo data which break syntax highlighting:
+addrSchema = `address { street:NAME no:NUM? }
 NAME : .*;
 NUM : [0-9]+[a-e]?;
 `;
 
-var perAddrSchema = `person { name:NAME addrs:[address]? }
+perAddrSchema = `person { name:NAME addrs:[address]? }
 address { street:NAME no:NUM? }
 NAME : .*;
 NUM : [0-9]+[a-e]?;
 `;
 
-var shexjSchema = `.TYPE type; # All objects have a type property corresponding to the production name, e.g. "Schema"
+shexjSchema = `.TYPE type; # All objects have a type property corresponding to the production name, e.g. "Schema"
 
 Schema           { prefixes:{PREFIX->IRI}? base:IRI? startActs:[SemAct]? start:shapeExpr? shapes:{shapeLabel->shapeExpr}? }
 shapeExpr        = ShapeOr | ShapeAnd | ShapeNot | NodeConstraint | Shape | ShapeRef | ShapeExternal;
@@ -79,218 +297,6 @@ EXPONENT 	 : [eE] [+-]? [0-9]+ ;
 LANGTAG          : '@' [a-zA-Z] + ('-' [a-zA-Z0-9] +)* ;
 `; // '
 
-var demos = {
-  "address": {
-    schema: addrSchema,
-    passes: {
-      "complete": { "street":"Elm", "no":"123b" },
-      "just name": { "street":"Elm" },
-    },
-    fails: {
-      "missing street": { no: 123 },
-      "extra property": { "street":"Elm", "no":"123b", "x": "y" }
-    }
-  },
-  "person with address": {
-    schema: perAddrSchema,
-    passes: {
-      "no addrs": { name: "Bob" },
-      "empty addresses": { name: "Bob", addrs: [] },
-      "1 address": { name: "Bob", addrs: [
-        { "street":"Elm", "no":"123b" }
-      ] },
-      "2 addresses": { name: "Bob", addrs: [
-        { "street":"Elm", "no":"123b" },
-        { "street":"Forest", "no":"18" }
-      ] },
-    },
-    fails: {
-      "missing name": { addrs: [
-        { "street":"Elm", "no":"123b" }
-      ] },
-      "bad address": { name: "Bob", addrs: [
-        { "street":"Elm", "no":"123b", "x": "y" }
-      ] },
-      "bad last addresses": { name: "Bob", addrs: [
-        { "street":"Elm", "no":"123b" },
-        { "no":"18" }
-      ] },
-    }
-  },
-  "ShExJ": {
-    schema: shexjSchema,
-    passes: {
-      "1card25":{
-  "type": "Schema",
-  "shapes":{
-    "http://a.example/S1": {
-      "type": "Shape",
-      "expression": {
-        "type": "TripleConstraint",
-        "predicate": "http://a.example/p1",
-        "min": 2, "max": 5
-      }
-    }
-  }
-},
-      "1bnodeRefORRefMinlength": {
-  "type": "Schema",
-  "shapes": {
-    "http://a.example/S1": {
-      "type": "Shape",
-      "expression": {
-        "type": "TripleConstraint",
-        "predicate": "http://a.example/p1",
-        "valueExpr": {
-          "type": "ShapeOr",
-          "shapeExprs": [
-            {
-              "type": "ShapeAnd",
-              "shapeExprs": [
-                {
-                  "type": "NodeConstraint",
-                  "nodeKind": "bnode"
-                },
-                {
-                  "type": "ShapeRef",
-                  "reference": "http://a.example/S1"
-                }
-              ]
-            },
-            {
-              "type": "ShapeAnd",
-              "shapeExprs": [
-                {
-                  "type": "NodeConstraint",
-                  "minlength": 5
-                },
-                {
-                  "type": "ShapeRef",
-                  "reference": "http://a.example/S1"
-                }
-              ]
-            }
-          ]
-        }
-      }
-    }
-  }
-}
-    },
-    fails: {
-      "misplaced attr":{
-  "type": "Schema",
-  "shapes":{
-    "http://a.example/S1": {
-      "type": "Shape", "max": 5,
-      "expression": {
-        "type": "TripleConstraint",
-        "predicate": "http://a.example/p1",
-        "min": 2
-      }
-    }
-  }
-},
-      "wrong type": {
-  "type": "Schema",
-  "shapes": {
-    "http://a.example/S1": {
-      "type": "Shape",
-      "expression": {
-        "type": "TripleConstraint",
-        "predicate": "http://a.example/p1",
-        "valueExpr": {
-          "type": "ShapeOr",
-          "shapeExprs": [
-            {
-              "type": "ShapeAnd",
-              "shapeExprs": [
-                {
-                  "type": "NodeConstraint",
-                  "nodeKind": "bnode"
-                },
-                {
-                  "type": "ShapeRef",
-                  "reference": "http://a.example/S1"
-                }
-              ]
-            },
-            {
-              "type": "Inclusion",
-              "shapeExprs": [
-                {
-                  "type": "NodeConstraint",
-                  "minlength": 5
-                },
-                {
-                  "type": "ShapeRef",
-                  "reference": "http://a.example/S1"
-                }
-              ]
-            }
-          ]
-        }
-      }
-    }
-  }
-},
-    }
-  },
-};
+prepareDemos();
 
-function load (selector, obj, func) {
-  $(selector).empty();
-  Object.keys(obj).forEach(k => {
-    var li = $('<li><a href="#">' + k + '</li>');
-    li.on("click", () => {
-      func(k, obj[k], li);
-    });
-    $(selector).append(li);
-  });
-}
-
-$("#data .passes ul, #data .fails ul").empty();
-load("#schema .examples ul", demos, pickSchema);
-
-function pickSchema (name, schemaTest, elt) {
-  $("#results").text("");
-  $("#data textarea").val("");
-  $("#schema .status").text(name);
-  $("#schema textarea").val(schemaTest.schema);
-  $("#data .passes p:first").text("Passing:");
-  load("#data .passes ul", schemaTest.passes, pickData);
-  $("#data .fails p:first").text("Failing:");
-  load("#data .fails ul", schemaTest.fails, pickData);
-}
-
-function pickData (name, dataTest, elt) {
-  $("#data .status").text(name);
-  $("#data textarea").val(JSON.stringify(dataTest, null, "  "));
-  validate();
-  // load("#data .examples", data.passes, dfill);
-}
-
-$("#validate").on("click", validate);
-
-function validate () {
-  var schemaText = $("#schema textarea").val();
-  var parsed = parser.parse(schemaText);
-  var schema = Schema(parsed);
-  var data = JSON.parse($("#data textarea").val());
-  var errors = schema.validator().validate(data);
-  $("#results").text(errors.join("\n"));
-}
-
-$("#clear").on("click", clear);
-
-function clear () {
-  $("#schema textarea").val("");
-  $("#data textarea").val("");
-  $("#data .passes p:first").text("");
-  $("#data .fails p:first").text("");
-  $("#data .passes ul, #data .fails ul").empty();
-  $("#schema .status").text(" ");
-  $("#data .status").text(" ");
-  $("#results").text("");
-}
 
